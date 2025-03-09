c0a82dcf47b06654d48cf491bd0647a1a424cccf
burlak1n <roma.s-2012@yandex.ru>
2025-03-09 11:41:16 +0300

    Рефакторинг инициализации административной панели SQLAdmin

---
+++ app/cms/router.py
@@ -0,0 +1,9 @@
+from sqladmin import Admin
+from app.dao.database import engine
+from app.cms.views import UserAdmin, RoleAdmin
+
+def init_admin(app, base_url: str = "/admin"):
+    """Инициализация SQLAdmin"""
+    admin = Admin(app, engine, base_url=base_url)
+    admin.add_view(UserAdmin)
+    admin.add_view(RoleAdmin)
