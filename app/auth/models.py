1f568890dcb179dbd58e0fb932c782fe4b457e57
burlak1n <roma.s-2012@yandex.ru>
2025-03-11 11:39:11 +0300

    Добавлены модели Command и CommandUser для управления командами пользователей

--- app/auth/models.py
+++ app/auth/models.py
@@ -4,6 +4,7 @@ from app.dao.database import Base, str_uniq, int_uniq
 
 
 class Role(Base):
+    # "guest" "organizer" "insider" 
     name: Mapped[str_uniq]
     users: Mapped[list["User"]] = relationship(back_populates="role")
 
@@ -18,6 +19,27 @@ class User(Base):
 
     role_id: Mapped[int] = mapped_column(ForeignKey('roles.id'), default=1, server_default=text("1"))
     role: Mapped["Role"] = relationship("Role", back_populates="users", lazy="joined")
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(id={self.id})"
+
+class Command(Base):
+    name: Mapped[str]
+    captain_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
+    captain: Mapped["User"] = relationship("User", back_populates="commands", lazy="joined")
+
+    count_users: Mapped[int] = mapped_column(default=1, )
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(id={self.id})"
+
+
+class CommandUser(Base):
+    command_id: Mapped[int] = mapped_column(ForeignKey('commands.id'), nullable=False)
+    command: Mapped["Command"] = relationship("Command", back_populates="users", lazy="joined")
+
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
+    user: Mapped["User"] = relationship("User", back_populates="commands", lazy="joined")
 
     def __repr__(self):
         return f"{self.__class__.__name__}(id={self.id})"
